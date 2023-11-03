2e68bb545913af2541a1a1be7588c40e181ded4d
Pablo Vergara <pablo.j98@gmail.com>
2023-11-03 11:51:27 -0400

    Add logical to select list on text questions

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -4,42 +4,47 @@ function Question({question}) {
 	
 	// text
   if(question.type === 'text'){
+		const datasetId = question.id+'-options'
     return (
       <div className="flex flex-col p-1     md:mb-3">
-	<label htmlFor={question.id}>{question.question}</label>
-	<input 
-		className="border p-1 rounded"
-		type={question.type} 
-		placeholder={question.placeholder} 
-		min={0} 
-		id={question.id}
-		value={undefined}
-	/>
+				<label htmlFor={question.id}>{question.question}</label>
+				<input 
+					className="border p-1 rounded"
+					type={question.type} 
+					placeholder={question.placeholder} 
+					min={0}
+					list={datasetId} 
+					id={question.id}
+					value={undefined}
+				/>
+				<datalist id={datasetId}>
+					{
+						(question.options.length != 0) && question.options.map( ( option, index ) => {
+							return <option key={option + index} value={option}></option>
+						})
+					}
+				</datalist>
       </div>
-    )
-  }
+  )}
 
 	// Number
    else if( question.type === 'number'){
      return(
-	<div className="flex flex-col p-1     md:mb-3">
-	  <label
-	    className="text-md font-mono" 
-	    htmlFor={question.id}
-	  >{question.question}</label>
-	  <input 
-	    className="border p-1 rounded"
-	    type={question.type}  
-	    min={0}
-	    placeholder={0} 
-	    id={question.id}
-	    value={undefined}
-	  />
-	</div>
-	)
-
-   return <h1>{question.id}</h1>
-  }
+			<div className="flex flex-col p-1     md:mb-3">
+				<label
+					className="text-md font-mono" 
+					htmlFor={question.id}
+				>{question.question}</label>
+				<input 
+					className="border p-1 rounded"
+					type={question.type}  
+					min={0}
+					placeholder={0} 
+					id={question.id}
+					value={undefined}
+				/>
+			</div>
+		)}
 
 	return null
 }
--- src/util/script.js
+++ src/util/script.js
@@ -4,7 +4,7 @@ const data ={
     { id:'name', question:'Clients Name', template:`Clients name: `, type:'text', placeholder:'Name ...', options:[], required:false }, // tested
     { id:'address', question:'Address', template:`Address: `, type:'text', placeholder:'Address ...', required:true, options:[], required:false }, // tested
     { id:'price', question:'Asking price', template:`Asking Price: `, type:'number' }, // tested
-    { id:'reason', question:'Reason', template:`Reason: `, type:'text', placeholder:'The reason is ...', options:[], required:false }, // tested
+    { id:'reason', question:'Reason', template:`Reason: `, type:'text', placeholder:'The reason is ...', options:['Downsizing','moving to another state'], required:false }, // tested
     { id:'list', question:'Is the property listed on the market?', template:`Listed: `, type:'radio', placeholder:'', name:'listing', options:[], required:false }, // tested
     { id:'rent', question:'Is the property rented?', template:`Rented: `, type:'radio', placeholder:'', name:'renting', options:[], required:false }, // tested
     { id:'lease', question:'What kind of lease do the tenants have?', template:`Type of lease: `, type:'radio-name', placeholder:'', name:'lease', required:false, options:['Anual Lease','Monthly']}, //tested 
